cf2ec50fce1b8cd36c47559827dab822897c6ef4
quaofGOD <69598671+quaofGOD@users.noreply.github.com>
2023-04-20 10:54:38 +0300

    first

---
+++ analysis.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv("veri-seti.csv")
+
+toplam_gelir = df["Gelir"].sum()
+toplam_gider = df["Gider"].sum()
+
+
+
+kar = toplam_gelir - toplam_gider
+df.plot(kind="bar", x="Ay", y=["Gelir", "Gider"])
+plt.title("Gelirler ve Giderler")
+plt.xlabel("Ay")
+plt.ylabel("Tutar")
+plt.show()
+
+
+if kar > 0:
+    print(f"Kar: {kar} TL")
+else:
+    print(f"Zarar: {kar} TL")
